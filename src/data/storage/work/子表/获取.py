0d2ab73d7f8cb005e42f51df62948544ce3a6870
user98643 <wcj985@qq.com>
2025-05-13 19:54:55 +0800

    图片识别品名修正自动动态获取俩子表sheet名作为标准产品名

--- src/data/storage/work/子表/获取.py
+++ src/data/storage/work/子表/获取.py
@@ -1,6 +1,5 @@
 import os
 import xlrd
-from pyperclip import copy
 
 # 获取当前目录下所有 .xls 文件
 xls_files = [f for f in os.listdir('.') if f.endswith('.xls')]
@@ -9,7 +8,6 @@ for file in xls_files:
     try:
         workbook = xlrd.open_workbook(file)
         sheet_names = workbook.sheet_names()
-        copy(str(sheet_names))
         print(f"{file} 的 sheets: {sheet_names}")
     except Exception as e:
         print(f"{file} 打开失败: {e}")
